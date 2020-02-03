bc0ffaf2204215b25dc90b9fea5a5ff0be2b8774
olkhovsky <olkhovsky11@mail.ru>
2020-02-03 16:34:40 +0300

    save

---
+++ src/test/java/Sem1/ProgramTest.java
@@ -0,0 +1,16 @@
+package Sem1;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.lang.reflect.Array;
+
+import static org.junit.Assert.*;
+
+public class ProgramTest {
+    @Test
+    public class CatBoris() {
+        String[1] array;
+        Assert.assertArrayEquals("Boris can eat milk, red apple", Program.main(new Strzing[0]));
+    }
+}
